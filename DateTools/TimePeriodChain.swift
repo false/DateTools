b6447afcf3d3fbc88bb36bd26922ebcf51b41a1d
Tanguy G <tanguy.gourvez@gmail.com>
2016-09-12 16:05:07 +0200

    Internal to public : buildable

--- DateTools/TimePeriodChain.swift
+++ DateTools/TimePeriodChain.swift
@@ -35,15 +35,15 @@ public class TimePeriodChain: TimePeriodGroup {
         
     }
     
-    internal override func map<T>(_ transform: (TimePeriodProtocol) throws -> T) rethrows -> [T] {
+    public override func map<T>(_ transform: (TimePeriodProtocol) throws -> T) rethrows -> [T] {
         return try periods.map(transform)
     }
     
-    internal override func filter(_ isIncluded: (TimePeriodProtocol) throws -> Bool) rethrows -> [TimePeriodProtocol] {
+    public override func filter(_ isIncluded: (TimePeriodProtocol) throws -> Bool) rethrows -> [TimePeriodProtocol] {
         return try periods.filter(isIncluded)
     }
     
-    internal override func reduce<Result>(_ initialResult: Result, _ nextPartialResult: (Result, TimePeriodProtocol) throws -> Result) rethrows -> Result {
+    public override func reduce<Result>(_ initialResult: Result, _ nextPartialResult: (Result, TimePeriodProtocol) throws -> Result) rethrows -> Result {
         return try periods.reduce(initialResult, nextPartialResult)
     }
     
